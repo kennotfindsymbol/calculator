a6a20b804663e25cf5933ee81e9a0e7711d62969
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-01-21 16:32:54 +0800

    Add calculating function
    Only works for single calculation

---
+++ script.js
@@ -0,0 +1,54 @@
+let lhs = 0;
+let rhs = 0;
+let isCurrentLeft = true;
+let currOp = 'add';
+
+const ac = document.getElementById('ac');
+ac.addEventListener('click', () => {
+    lhs = 0;
+    rhs = 0;
+    isCurrentLeft = true;
+    currOp = 'add';
+    document.querySelector('h1').textContent = 0;
+});
+
+const equal = document.getElementById('equal');
+equal.addEventListener('click', () => {
+    const result = calculate(lhs, rhs, currOp);
+    lhs = result;
+    document.querySelector('h1').textContent = result;
+});
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if(isCurrentLeft){
+            lhs = lhs * 10 + +number.textContent;
+            document.querySelector('h1').textContent = lhs;
+        } else {
+            rhs = rhs * 10 + +number.textContent;
+            document.querySelector('h1').textContent = rhs;
+        }
+    })
+})
+
+const ops = document.querySelectorAll('.op');
+ops.forEach(op => {
+    op.addEventListener('click', () => {
+        currOp = op.id;
+        isCurrentLeft = false;
+    })
+})
+
+function calculate(lhs, rhs, op) {
+    switch (op) {
+        case 'add':
+            return lhs + rhs;
+        case 'subtract':
+            return lhs - rhs;
+        case 'multiply':
+            return lhs * rhs;
+        case 'divide':
+            return lhs / rhs;
+    }
+}
